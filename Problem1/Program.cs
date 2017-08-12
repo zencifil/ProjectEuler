d3b603e78d760adc9e2f86d6afc48ce6294c97b8
Savaş Cilve <savascilve@outlook.com>
2017-08-12 14:09:03 +0100

    Problem 1 is done!

---
+++ Problem1/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * 
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ * 
+ */
+
+using System;
+
+namespace Problem1 {
+    public static class Program {
+        static void Main(string[] args) {
+            var limit = 999;
+            var sum = SumOfIntegers(limit);
+            Console.WriteLine("Sum is " + sum);
+            Console.ReadLine();
+        }
+
+        public static int SumOfIntegers(int limit) {
+            var sum = 0;
+            for (var i = 1; i <= limit; i++) {
+                if (IsDivisibleBy3(i) || IsDivisibleBy5(i))
+                    sum += i;
+            }
+
+            return sum;
+        }
+
+        public static bool IsDivisibleBy3(int number) {
+            return number % 3 == 0;
+        }
+
+        public static bool IsDivisibleBy5(int number) {
+            return number % 5 == 0;
+        }
+    }
+}
---
+++ Problem1_Test/P1_Test.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem1_Test {
+    [TestClass]
+    public class P1_Test {
+        [TestMethod]
+        public void EightyFiveIsNotDivisibleBy3() {
+            //Arrange
+            var numberToDivide = 85;
+
+            //Act
+            var isDivisible = Problem1.Program.IsDivisibleBy3(numberToDivide);
+
+            //Assert
+            Assert.AreEqual(false, isDivisible, "85 is not divisible by 3!!");
+        }
+
+        [TestMethod]
+        public void SixtyThreeIsDivisibleBy3() {
+            //Arrange
+            var numberToDivide = 63;
+
+            //Act
+            var isDivisible = Problem1.Program.IsDivisibleBy3(numberToDivide);
+
+            //Assert
+            Assert.AreEqual(true, isDivisible, "63 is divisible by 3!!");
+        }
+
+        [TestMethod]
+        public void NinetyThreeIsNotDivisibleBy5() {
+            //Arrange
+            var numberToDivide = 93;
+
+            //Act
+            var isDivisible = Problem1.Program.IsDivisibleBy5(numberToDivide);
+
+            //Assert
+            Assert.AreEqual(false, isDivisible, "93 is not divisible by 5!!");
+        }
+
+        [TestMethod]
+        public void FiftyFiveIsDivisibleBy5() {
+            //Arrange
+            var numberToDivide = 55;
+
+            //Act
+            var isDivisible = Problem1.Program.IsDivisibleBy5(numberToDivide);
+
+            //Assert
+            Assert.AreEqual(true, isDivisible, "55 is not divisible by 5!!");
+        }
+
+        //[TestMethod]
+        //public void SumOfFirst100IntegersShouldBe5050() {
+        //    //Arrange
+        //    var limit = 100;
+
+        //    //Act
+        //    var total = Problem1.Program.SumOfIntegers(limit);
+
+        //    //Assert
+        //    Assert.AreEqual(5050, total, "Sum of first 100 integers should be 5050!!");
+        //}
+
+        [TestMethod]
+        public void SumOfMultiplesOf3Or5Below10_ShouldBe23() {
+            //Arrange
+            var limit = 9;
+
+            //Act
+            var total = Problem1.Program.SumOfIntegers(limit);
+
+            //Assert
+            Assert.AreEqual(23, total, "Sum of multiples of 3 or 5 below 10 must be 23!");
+        }
+    }
+}
