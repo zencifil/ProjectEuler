43827bf00722dd705824562da6ec032d582627a8
Savaş Cilve <savascilve@outlook.com>
2017-08-12 15:26:50 +0100

    Problem2 is added

---
+++ Problem2_Test/P2_Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem2_Test {
+
+    [TestClass]
+    public class P2_Test {
+
+        [TestMethod]
+        public void IsEven_Given10_ShouldReturnTrue() {
+            //Arrange
+            var given = 10;
+
+            //Act
+            var isEven = Problem2.Program.IsEven(given);
+
+            //Assert
+            Assert.IsTrue(isEven, "10 is even!!!");
+        }
+
+        //[TestMethod]
+        //public void SumFibonacciSequenceForBelow100StartingFrom1_ShouldReturn231() {
+        //    //Arrange
+        //    var limit = 100;
+
+        //    //Act
+        //    var total = Problem2.Program.SumFibonacciSequence(limit);
+
+        //    //Assert
+        //    Assert.AreEqual(231, total);
+        //}
+
+        [TestMethod]
+        public void SumFibonacciSeqEvenNumsForBelow100_ShouldReturn44() {
+            //Arrange
+            var limit = 100;
+
+            //Act
+            var total = Problem2.Program.SumFibonacciSequence(limit);
+
+            //Assert
+            Assert.AreEqual(44, total);
+        }
+    }
+}
---
+++ Problem2_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Problem2_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Problem2_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e561d6af-0d63-4b93-934f-b9150f07aea4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
